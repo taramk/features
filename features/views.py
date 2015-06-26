052390553636348aa2501aaadd350f28aff7ec94
Tara Kirkland <tara@chartio.com>
2015-06-25 22:30:21 -0700

    adding customers to requests is working!

--- features/views.py
+++ features/views.py
@@ -13,9 +13,17 @@ def index(request):
     return render(request, 'features/index.html', context)
 
 
-# def detail(request, feature_id):
-#     feature = get_object_or_404(Feature, pk=feature_id)
-#     return render(request, 'features/detail.html', {'feature': feature})
+def detail(request, feature_id):
+    feature = get_object_or_404(Feature, pk=feature_id)
+    if request.method == 'POST':  # If the form has been submitted...
+        form = AddCustomerForm(request.POST)  # A form bound to the POST data
+        if form.is_valid():  # All validation rules pass
+            customer, _ = Customer.objects.get_or_create(email=form.cleaned_data['customer_email'])
+            feature.customers.add(customer)
+            return render(request, 'features/detail.html', {'feature': feature})
+    else:
+        form = AddCustomerForm()  # An unbound form
+    return render(request, 'features/detail.html', {'feature': feature})
 
 
 def customers(request, feature_id):
@@ -33,18 +41,4 @@ def create_feature(request):
             return HttpResponseRedirect('/features/')
     else:
         form = FeatureForm()  # An unbound form
-
     return render(request, 'features/new-feature.html', {'form': form})
-
-
-def detail(request, feature_id):
-    feature = get_object_or_404(Feature, pk=feature_id)
-    if request.method == 'POST':  # If the form has been submitted...
-        form = AddCustomerForm(request.POST)  # A form bound to the POST data
-        if form.is_valid():  # All validation rules pass
-            customer, _ = Customer.objects.get_or_create(email=form.cleaned_data['customer_email'])
-            feature.customers.add(customer)
-            return HttpResponseRedirect('/features/')
-    else:
-        form = AddCustomerForm()  # An unbound form
-    return render(request, 'features/detail.html', {'feature': feature})
