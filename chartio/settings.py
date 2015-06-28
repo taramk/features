cc37e520799e084781f95fc7990e461831669423
Tara Kirkland <tara@chartio.com>
2015-06-28 14:41:50 -0700

    use remote database locally

--- chartio/settings.py
+++ chartio/settings.py
@@ -62,22 +62,12 @@ ROOT_URLCONF = 'chartio.urls'
 
 WSGI_APPLICATION = 'chartio.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {}
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+from local_settings import DATABASE_URL
+DATABASES['default'] = dj_database_url.config(default=DATABASE_URL)
 
 # Enable Connection Pooling
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
